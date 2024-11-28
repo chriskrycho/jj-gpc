381af0b218659ba6631ce5df96750cbaf62d5c0c
Chris Krycho <hello@chriskrycho.com>
2024-11-27 19:13:28 -0700

    Make the first fully-working version!
    This is fairly dumb, but it does the job: gets the summary messages for
    the changes between `trunk()` and `@` (or on some other revset), invokes
    ollama with a local model (currently hard coded, but I could make it
    configurable if I want later), and then uses that to create a bookmark
    and push with that.
    
    A few things I might do later (or not!):
    
    - Invoke jj in process rather than doing it by spawning the process.
      There is no convenient top level API to do this right now as far as I
      can tell; mayyyybe I can help with that?
    
    - Get the list of current branches and forbid those from being used as
      branch names in the prompt.
    
    - Improve on how the generation command actually works, operating at a
      lower level than the “give me a whole completion” chat interaction.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{self, Output};
+
+use clap::Parser as _;
+use lazy_static::lazy_static;
+use ollama_rs::{
+    generation::{completion::request::GenerationRequest, options::GenerationOptions},
+    Ollama,
+};
+use regex::Regex;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let args = Cli::parse();
+    let revision = args
+        .revision
+        .as_ref()
+        .map(|s| s.as_str())
+        .unwrap_or(DEFAULT_REV);
+
+    let commits = execute(process::Command::new("jj").args(&[
+        "log",
+        "-T",
+        LOG_TEMPLATE,
+        "--no-graph",
+        "-r",
+        revision,
+    ]));
+
+    let ollama = Ollama::default();
+    let prompt = format!("{LLM_PROMPT}\n\n```\n{commits}\n```");
+
+    let request = GenerationRequest::new(LLM_MODEL.into(), prompt)
+        .options(GenerationOptions::default().top_k(10));
+
+    let response = ollama
+        .generate(request)
+        .await
+        .unwrap_or_else(|e| panic!("{e}"))
+        .response;
+    let branch_name = SPACES.replace_all(response.trim(), "-");
+
+    if args.dry_run {
+        println!("[dry run] jj bookmark create {branch_name}");
+        println!("[dry run] jj git push --bookmark {branch_name}");
+        return;
+    }
+
+    let branch_output =
+        execute(process::Command::new("jj").args(&["bookmark", "create", &branch_name]));
+
+    println!("{branch_output}");
+
+    let push_output =
+        execute(process::Command::new("jj").args(&["git", "push", "--bookmark", &branch_name]));
+
+    println!("{push_output}");
 }
+
+fn execute(command: &mut process::Command) -> String {
+    let Output {
+        status,
+        stdout,
+        stderr,
+    } = command
+        .output()
+        .unwrap_or_else(|e| panic!("Could not execute command {command:?}.\nCause: {e}"));
+
+    if !status.success() {
+        eprintln!("{status}: {stderr:?}");
+        process::exit(status.code().unwrap_or(1));
+    }
+
+    String::from_utf8_lossy(&stdout).to_string()
+}
+
+/// Generate a branch name for use with jj.
+#[derive(clap::Parser, Debug)]
+struct Cli {
+    #[arg(short, long)]
+    revision: Option<String>,
+
+    #[arg(long = "dry-run", default_value = "false")]
+    dry_run: bool,
+}
+
+lazy_static! {
+    static ref SPACES: Regex = Regex::new(r"\s+").unwrap();
+}
+
+const DEFAULT_REV: &'static str = "trunk()..@";
+
+const LOG_TEMPLATE: &'static str = r#"if(description, description.first_line(), '') ++ "\n""#;
+
+const LLM_MODEL: &'static str = "llama3.2";
+const LLM_PROMPT: &'static str = "Summarizing *all* of these messages in a single phrase. The phrase should consist of 2–4-words, all lowercase. Do not mention branches. Do not include more words. Reply with only the phrase.";

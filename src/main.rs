073b9cab36ffb87bcb219a43bdd74555f7b0acc7
Chris Krycho <hello@chriskrycho.com>
2024-11-29 19:39:01 -0700

    0.3.0: substantially improve perf/weight ratio
    1. Use a *much* smaller model by default.
    2. Tweak the defaults for the system: slightly higher temperature, but
       much lower value for `top-p` and `top-k`, resulting in much more
       consistent output.
    3. Only allow it to return 10 tokens!
    4. Rewrite the prompt to be *much* more explicit with actual examples.
    
    As a bonus, add support for using a full log rather than a single-line
    log as input.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::process::{self, Output};
 
-use clap::{Parser as _, ValueEnum as _};
+use clap::Parser as _;
 use ollama_rs::{
     generation::{completion::request::GenerationRequest, options::GenerationOptions},
     Ollama,
@@ -10,17 +10,17 @@ use regex::Regex;
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     env_logger::init();
+
     let args = Cli::parse();
     let revset = format!("{}..{}", args.from, args.change);
 
-    let commits = execute(process::Command::new("jj").args(&[
-        "log",
-        "-T",
-        LOG_TEMPLATE,
-        "--no-graph",
-        "-r",
-        &revset,
-    ]));
+    let log_template = match args.log_format {
+        Some(LogFormat::Full) => LOG_FULL,
+        Some(LogFormat::OneLine) | None => LOG_ONE_LINE,
+    };
+
+    let commits =
+        execute(process::Command::new("jj").args(&["log", "-T", log_template, "-r", &revset]));
 
     if commits.trim().is_empty() {
         eprintln!("No commits to summarize");
@@ -28,28 +28,22 @@ async fn main() {
     }
 
     let ollama = Ollama::default();
-    let prompt = format!("{LLM_PROMPT}\n\n```\n{commits}\n```");
-
-    let model = args
-        .model
-        .to_possible_value()
-        .expect("there should always be a model value")
-        .get_name()
-        .to_owned();
-
-    let request = GenerationRequest::new(model, prompt).options(
+    let prompt = format!("{PROMPT_START}\n\n```\n{commits}\n```\n\n{PROMPT_END}");
     log::debug!("prompt: {prompt}");
+
+    let request = GenerationRequest::new(args.model, prompt.clone()).options(
         GenerationOptions::default()
             .top_k(args.top_k)
             .top_p(args.top_p)
-            .temperature(args.temperature),
+            .temperature(args.temperature)
+            .num_predict(10),
     );
 
-    let response = ollama
+    let generation_response = ollama
         .generate(request)
         .await
-        .unwrap_or_else(|e| panic!("{e}"))
-        .response;
+        .unwrap_or_else(|e| panic!("{e}"));
+    let response = generation_response.response.trim().trim_end_matches("-");
 
     let branch_name = Regex::new(r"\s+")
         .unwrap()
@@ -119,6 +113,9 @@ struct Cli {
     #[arg(default_value = "@")]
     change: String,
 
+    #[arg(long, value_enum)]
+    log_format: Option<LogFormat>,
+
     #[arg(short, long, default_value = "trunk()")]
     from: String,
 
@@ -132,32 +129,45 @@ struct Cli {
 
     /// The temperature of the model. Increasing the temperature will make the
     /// model answer more creatively.
-    #[arg(long, default_value = "0.8")]
+    #[arg(long, default_value = "2")]
     temperature: f32,
 
     /// Reduces the probability of generating nonsense. A higher value (e.g. 100)
     /// will give more diverse answers, while a lower.
-    #[arg(long, default_value = "40")]
+    #[arg(long, default_value = "20")]
     top_k: u32,
 
     /// Works together with top-k. A higher value (e.g., 0.95) will lead to more
     /// diverse text, while a lower value (e.g., 0.5) will generate more focused
     /// and conservative text.
-    #[arg(long, default_value = "0.9")]
+    #[arg(long, default_value = "0.7")]
     top_p: f32,
 
-    #[arg(long, value_enum, default_value = "llama3.2")]
-    model: Model,
+    /// Which model to use. Can be any model available in Ollama on your system.
+    ///
+    /// The model you choose to use will significantly alters the quality of the
+    /// output, so you may need to tune the parameters as well. If this is not a
+    /// model available in Ollama on your system, the request will fail.
+    #[arg(long, default_value = "phi3:3.8b-mini-4k-instruct-q4_K_S")]
+    model: String,
 }
 
-#[derive(clap::ValueEnum, Clone, Debug)]
-enum Model {
-    #[clap(name = "llama3.2")]
-    Llama3_2,
-    #[clap(name = "llama3.2:1b")]
-    Llama3_2_1b,
+#[derive(clap::ValueEnum, Debug, Clone)]
+enum LogFormat {
+    OneLine,
+    Full,
 }
 
-const LOG_TEMPLATE: &'static str = r#"if(description, description) ++ "\n\n---\n\n""#;
+const LOG_ONE_LINE: &'static str = r#"if(description, description.first_line(), '') ++ "\n""#;
+const LOG_FULL: &'static str = "builtin_log_compact_full_description";
 
-const LLM_PROMPT: &'static str = "Summarize all of these messages in a single phrase. The phrase should consist of 2–4-words, all lowercase. Do not mention branches. Do not include more words. Reply with only the phrase.";
+const PROMPT_START: &'static str = "Here is the Git commit log for this branch:";
+const PROMPT_END: &'static str = r#"Reply with the shortest descriptive branch name (*not* a pull request description, just a branch name) for a Git branch containing these commits, using no more than 7 lowercase words separated by hyphens.
+
+- A good branch name is always derived from the summary of changes in the log.
+- Never return branch names which:
+    - have too little information, like `ab-1234`
+    - are too long, for example `this-branch-name-is-fifteen-words-long-instead-of-maxxing-out-at-seven-as-instructed`
+    - simply include a date
+- Do not include any explanation, only the branch name.
+"#;
